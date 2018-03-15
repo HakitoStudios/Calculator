fc8164c9c549c43cffb1a2d0af5c4100c4d3c5da
Your Name <you@example.com>
2018-03-15 09:54:57 +0200

    Added empty line

--- app/src/main/java/com/example/school/myapplication/HelpActivity.java
+++ app/src/main/java/com/example/school/myapplication/HelpActivity.java
@@ -8,6 +8,7 @@ public class HelpActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_help);
     }
 }
